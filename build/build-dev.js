1ed4e204db74dd6bd4625d23ccf3ca232fe97b6f
Matt Bell <mappum@gmail.com>
2017-04-03 13:25:02 -0700

    Added build-dev script

---
+++ build/build-dev.js
@@ -0,0 +1,39 @@
+// https://github.com/shelljs/shelljs
+require('./check-versions')()
+require('shelljs/global')
+env.NODE_ENV = 'development'
+
+var path = require('path')
+var config = require('../config')
+var ora = require('ora')
+var webpack = require('webpack')
+var webpackConfig = require('./webpack.dev.conf')
+
+console.log(
+  '  Tip:\n' +
+  '  Built files are meant to be served over an HTTP server.\n' +
+  '  Opening index.html over file:// won\'t work.\n'
+)
+
+var spinner = ora('building for development...')
+spinner.start()
+
+var assetsTmpPath = path.join(config.build.assetsRoot, config.build.assetsTmpDirectory)
+rm('-rf', assetsTmpPath)
+mkdir('-p', assetsTmpPath)
+cp('-R', 'static/', assetsTmpPath)
+
+webpack(webpackConfig, function (err, stats) {
+  var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
+  cp('-R', assetsTmpPath, assetsPath)
+
+  spinner.stop()
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n')
+})
