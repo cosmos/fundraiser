182ecf4aa5575f85c35c77e1b655d64a5debe283
Matt Bell <mappum@gmail.com>
2017-04-04 11:05:19 -0700

    Fixed dev config for COSMOS_BASE_URL not set by env var

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,8 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
+let baseUrl = process.env.COSMOS_BASE_URL
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  COSMOS_BASE_URL: `"${process.env.COSMOS_BASE_URL}"`
+  COSMOS_BASE_URL: baseUrl ? `"${baseUrl}"` : null
 })
