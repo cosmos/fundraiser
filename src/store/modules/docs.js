1dc649fea38dbc39cb69fa326c2e253a68757853
Peng Zhong <peng@nylira.com>
2017-04-05 11:51:18 +0800

    update docs links

--- src/store/modules/docs.js
+++ src/store/modules/docs.js
@@ -1,7 +1,9 @@
 const state = {
   btc: 'https://github.com/cosmos/cosmos/raw/master/fundraiser/COSMOS%20FUNDRAISER%20-%20BTC%20WEB.pdf',
-  eth: 'https://github.com/cosmos/cosmos/raw/master/fundraiser/COSMOS%20FUNDRAISER%20-%20ETH%20WEB.pdf',
-  terms: 'https://github.com/cosmos/cosmos/raw/master/fundraiser/Interchain%20Cosmos%20Contribution%20Terms%20-%20Penultimate%20Draft.pdf'
+  cli: 'https://github.com/cosmos/fundraiser-cli',
+  ethWeb: 'https://github.com/cosmos/cosmos/raw/master/fundraiser/COSMOS%20FUNDRAISER%20-%20ETH%20WEB.pdf',
+  ethMist: 'https://github.com/cosmos/cosmos/raw/master/fundraiser/COSMOS%20FUNDRAISER%20-%20ETH%20MIST.pdf',
+  terms: 'https://github.com/cosmos/cosmos/raw/master/fundraiser/Interchain%20Cosmos%20Contribution%20Terms%20-%20FINAL.pdf'
 }
 
 export default {
